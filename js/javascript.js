d5cd7fae1af62a9bbf4cb07145678846661d5e76
Dennis Ozmert <94586639+dozmert@users.noreply.github.com>
2022-07-27 09:40:26 +1000

    Updates

--- js/javascript.js
+++ js/javascript.js
@@ -141,8 +141,124 @@ function menuButton9() {
 	if (varCont9.style.display === "none")
 		{varCont9.style.display = "block";varCont2.style.display = "none"; varCont3.style.display = "none";varCont4.style.display = "none"; varCont5.style.display = "none";varCont6.style.display = "none"; varCont7.style.display = "none";varCont8.style.display = "none"; varCont1.style.display = "none";}
 };
-
-
+// Content 1 Menu button functions
+function contentMenuButton1() {
+	var varCont1 = document.getElementById("content-1-1");
+	var varCont2 = document.getElementById("content-1-2");
+	var varCont3 = document.getElementById("content-1-3");
+	var varCont4 = document.getElementById("content-1-4");
+	var varCont5 = document.getElementById("content-1-5");
+	var varCont6 = document.getElementById("content-1-6");
+	var varCont7 = document.getElementById("content-1-7");
+	var varCont8 = document.getElementById("content-1-8");
+	var varCont9 = document.getElementById("content-1-9");
+	if (varCont1.style.display === "none")
+		{varCont1.style.display = "block"; varCont2.style.display = "none"; varCont3.style.display = "none"; varCont4.style.display = "none"; varCont5.style.display = "none"; varCont6.style.display = "none"; varCont7.style.display = "none"; varCont8.style.display = "none"; varCont9.style.display = "none";}
+};
+function contentMenuButton2() {
+	var varCont1 = document.getElementById("content-1-1"); 
+	var varCont2 = document.getElementById("content-1-2"); 
+	var varCont3 = document.getElementById("content-1-3"); 
+	var varCont4 = document.getElementById("content-1-4"); 
+	var varCont5 = document.getElementById("content-1-5"); 
+	var varCont6 = document.getElementById("content-1-6"); 
+	var varCont7 = document.getElementById("content-1-7"); 
+	var varCont8 = document.getElementById("content-1-8"); 
+	var varCont9 = document.getElementById("content-1-9"); 
+	if (varCont2.style.display === "none")
+		{varCont2.style.display = "block";varCont1.style.display = "none"; varCont3.style.display = "none";varCont4.style.display = "none"; varCont5.style.display = "none";varCont6.style.display = "none"; varCont7.style.display = "none";varCont8.style.display = "none"; varCont9.style.display = "none";}
+};
+function contentMenuButton3() {
+	var varCont1 = document.getElementById("content-1-1"); 
+	var varCont2 = document.getElementById("content-1-2"); 
+	var varCont3 = document.getElementById("content-1-3"); 
+	var varCont4 = document.getElementById("content-1-4"); 
+	var varCont5 = document.getElementById("content-1-5"); 
+	var varCont6 = document.getElementById("content-1-6"); 
+	var varCont7 = document.getElementById("content-1-7"); 
+	var varCont8 = document.getElementById("content-1-8"); 
+	var varCont9 = document.getElementById("content-1-9"); 
+	if (varCont3.style.display === "none")
+		{varCont3.style.display = "block";varCont2.style.display = "none"; varCont1.style.display = "none";varCont4.style.display = "none"; varCont5.style.display = "none";varCont6.style.display = "none"; varCont7.style.display = "none";varCont8.style.display = "none"; varCont9.style.display = "none";}
+};
+function contentMenuButton4() {
+	var varCont1 = document.getElementById("content-1-1"); 
+	var varCont2 = document.getElementById("content-1-2"); 
+	var varCont3 = document.getElementById("content-1-3"); 
+	var varCont4 = document.getElementById("content-1-4"); 
+	var varCont5 = document.getElementById("content-1-5"); 
+	var varCont6 = document.getElementById("content-1-6"); 
+	var varCont7 = document.getElementById("content-1-7"); 
+	var varCont8 = document.getElementById("content-1-8"); 
+	var varCont9 = document.getElementById("content-1-9"); 
+	if (varCont4.style.display === "none")
+		{varCont4.style.display = "block";varCont2.style.display = "none"; varCont3.style.display = "none";varCont1.style.display = "none"; varCont5.style.display = "none";varCont6.style.display = "none"; varCont7.style.display = "none";varCont8.style.display = "none"; varCont9.style.display = "none";}
+};
+function contentMenuButton5() {
+	var varCont1 = document.getElementById("content-1-1"); 
+	var varCont2 = document.getElementById("content-1-2"); 
+	var varCont3 = document.getElementById("content-1-3"); 
+	var varCont4 = document.getElementById("content-1-4"); 
+	var varCont5 = document.getElementById("content-1-5"); 
+	var varCont6 = document.getElementById("content-1-6"); 
+	var varCont7 = document.getElementById("content-1-7"); 
+	var varCont8 = document.getElementById("content-1-8"); 
+	var varCont9 = document.getElementById("content-1-9"); 
+	if (varCont5.style.display === "none")
+		{varCont5.style.display = "block";varCont2.style.display = "none"; varCont3.style.display = "none";varCont4.style.display = "none"; varCont1.style.display = "none";varCont6.style.display = "none"; varCont7.style.display = "none";varCont8.style.display = "none"; varCont9.style.display = "none";}
+};
+function contentMenuButton6() {
+	var varCont1 = document.getElementById("content-1-1"); 
+	var varCont2 = document.getElementById("content-1-2"); 
+	var varCont3 = document.getElementById("content-1-3"); 
+	var varCont4 = document.getElementById("content-1-4"); 
+	var varCont5 = document.getElementById("content-1-5"); 
+	var varCont6 = document.getElementById("content-1-6"); 
+	var varCont7 = document.getElementById("content-1-7"); 
+	var varCont8 = document.getElementById("content-1-8"); 
+	var varCont9 = document.getElementById("content-1-9"); 
+	if (varCont6.style.display === "none")
+		{varCont6.style.display = "block";varCont2.style.display = "none"; varCont3.style.display = "none";varCont4.style.display = "none"; varCont5.style.display = "none";varCont1.style.display = "none"; varCont7.style.display = "none";varCont8.style.display = "none"; varCont9.style.display = "none";}
+};
+function contentMenuButton7() {
+	var varCont1 = document.getElementById("content-1-1"); 
+	var varCont2 = document.getElementById("content-1-2"); 
+	var varCont3 = document.getElementById("content-1-3"); 
+	var varCont4 = document.getElementById("content-1-4"); 
+	var varCont5 = document.getElementById("content-1-5"); 
+	var varCont6 = document.getElementById("content-1-6"); 
+	var varCont7 = document.getElementById("content-1-7"); 
+	var varCont8 = document.getElementById("content-1-8"); 
+	var varCont9 = document.getElementById("content-1-9"); 
+	if (varCont7.style.display === "none")
+		{varCont7.style.display = "block";varCont2.style.display = "none"; varCont3.style.display = "none";varCont4.style.display = "none"; varCont5.style.display = "none";varCont6.style.display = "none"; varCont1.style.display = "none";varCont8.style.display = "none"; varCont9.style.display = "none";}
+};
+function contentMenuButton8() {
+	var varCont1 = document.getElementById("content-1-1"); 
+	var varCont2 = document.getElementById("content-1-2"); 
+	var varCont3 = document.getElementById("content-1-3"); 
+	var varCont4 = document.getElementById("content-1-4"); 
+	var varCont5 = document.getElementById("content-1-5"); 
+	var varCont6 = document.getElementById("content-1-6"); 
+	var varCont7 = document.getElementById("content-1-7"); 
+	var varCont8 = document.getElementById("content-1-8"); 
+	var varCont9 = document.getElementById("content-1-9"); 
+	if (varCont8.style.display === "none")
+		{varCont8.style.display = "block";varCont2.style.display = "none"; varCont3.style.display = "none";varCont4.style.display = "none"; varCont5.style.display = "none";varCont6.style.display = "none"; varCont7.style.display = "none";varCont1.style.display = "none"; varCont9.style.display = "none";}
+};
+function contentMenuButton9() {
+	var varCont1 = document.getElementById("content-1-1"); 
+	var varCont2 = document.getElementById("content-1-2"); 
+	var varCont3 = document.getElementById("content-1-3"); 
+	var varCont4 = document.getElementById("content-1-4"); 
+	var varCont5 = document.getElementById("content-1-5"); 
+	var varCont6 = document.getElementById("content-1-6"); 
+	var varCont7 = document.getElementById("content-1-7"); 
+	var varCont8 = document.getElementById("content-1-8"); 
+	var varCont9 = document.getElementById("content-1-9"); 
+	if (varCont9.style.display === "none")
+		{varCont9.style.display = "block";varCont2.style.display = "none"; varCont3.style.display = "none";varCont4.style.display = "none"; varCont5.style.display = "none";varCont6.style.display = "none"; varCont7.style.display = "none";varCont8.style.display = "none"; varCont1.style.display = "none";}
+};
 
 
 
